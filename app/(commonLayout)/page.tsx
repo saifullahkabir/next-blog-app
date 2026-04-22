3a233c77d44d97177a958949219a4db15c348f14
SaifullahKabirRana <saifullahkabirranaa@gmail.com>
2026-04-22 19:29:10 +0600

    Implement env type safe with proper validation

--- app/(commonLayout)/page.tsx
+++ app/(commonLayout)/page.tsx
@@ -3,7 +3,7 @@ import { userService } from "@/services/user.service";
 
 export default async function Home() {
   const { data } = await userService.getSession();
-
+console.log(data);
   return (
     <div>
       <Button variant="outline">Click Me</Button>
--- services/user.service.ts
+++ services/user.service.ts
@@ -1,11 +1,14 @@
+import { env } from "@/env";
 import { cookies } from "next/headers";
+
+const AUTH_URL = env.AUTH_URL;
 
 export const userService = {
   getSession: async () => {
     try {
       const cookieStore = await cookies();
 
-      const res = await fetch("http://localhost:5000/api/auth/get-session", {
+      const res = await fetch(`${AUTH_URL}/get-session`, {
         headers: {
           Cookie: cookieStore.toString(),
         },
