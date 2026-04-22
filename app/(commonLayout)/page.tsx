a7c93aeb07ca3b7e8c5c22751b184c70dd01f0c0
SaifullahKabirRana <saifullahkabirranaa@gmail.com>
2026-04-23 03:00:51 +0600

    Implementation Private routes using proxy.ts

--- app/(commonLayout)/page.tsx
+++ app/(commonLayout)/page.tsx
@@ -1,9 +1,7 @@
 import { Button } from "@/components/ui/button";
-import { userService } from "@/services/user.service";
 
 export default async function Home() {
-  const { data } = await userService.getSession();
-console.log(data);
+
   return (
     <div>
       <Button variant="outline">Click Me</Button>
